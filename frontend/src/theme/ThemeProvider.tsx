f8a469c62b27bbd7fe1c6064fec327f8971b1232
swapnil chavan <swapnil740@gmail.com>
2025-08-15 23:01:43 +0530

    Create ThemeProvider.tsx file for theme provider component

---
+++ frontend/src/theme/ThemeProvider.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </MuiThemeProvider>
+  );
+};
